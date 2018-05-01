053968454c99a96bd7ade9a9dc2670c0b3d97bac
okputadora <okputadora@gmail.com>
2018-05-01 12:38:43 -0400

    added controls

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+const controls = [
+  { label: 'Salad', type: 'salad'},
+  { label: 'Salad', type: 'bacon'},
+  { label: 'Salad', type: 'cheese'},
+  { label: 'Salad', type: 'meat'},
+]
+const buildControls = (props) => (
+  <div className={classes.BuildControls}>
+    {controls.map(ctrl => {
+      return <BuildControl key={ctrl.label} label={ctrl.label} />
+    })}
+      </div>
+      );
+
+export default buildControls;
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import classes from './Burger.css'
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
 const burger = (props) => {
   // convert ingredients passed from BurgerBuilder state into array
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])].map((elem, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />
       })
     })
-    console.log(transformedIngredients)
+    // flatten the array
+    .reduce((arr, el) => { // arr is the previous val and el is the current val
+      return arr.concat(el)
+    }, []) // this empty array at the end is what we're building
+    if (transformedIngredients.length === 0){
+      transformedIngredients = <p>Please start adding ingredients</p>
+    }
   return(
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
