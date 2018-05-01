5277d5d753b562acab7cbc167cd173a557d18fa2
okputadora <okputadora@gmail.com>
2018-05-01 11:51:41 -0400

    added state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,11 +2,18 @@ import React from 'react'
 import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 const burger = (props) => {
+  // convert ingredients passed from BurgerBuilder state into array
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((elem, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />
+      })
+    })
+    console.log(transformedIngredients)
   return(
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
